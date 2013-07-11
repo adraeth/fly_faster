7cb3052cf510777fff599678b9c2e70ba70fb2b9
Jerzy Mirecki <jerzy.mirecki@gmail.com>
2013-07-11 23:59:37 +0200

    Remove commented-out get_all_flights pre-version

--- fly.rb
+++ fly.rb
@@ -1,23 +1,3 @@
-#def get_flight (airports, last_airport_index = nil, airport_count = 0, previous_continent = nil, continent_already_switched = false)
-
-=begin
-def get_flight (airports, airport_count = 0, options = {})
-  airports.each_with_index do |airport, index|
-    unless options[:continent_already_switched] && options[:previous_continent] != airport[:continent]
-      if airport_count == 3
-        puts "#{airport[:code]} (#{airport[:continent]}, #{index}, #{airport_count})"
-      else
-        print "#{airport[:code]} (#{airport[:continent]}, #{index}, #{airport_count}) - " if airport_count > 1
-        continent_switched = options[:previous_continent] == airport[:continent]
-        get_flight airports, airport_count + 1, {last_airport_index: index,
-                                               previous_continent: airport[:continent],
-                                               continent_already_switched: continent_switched}
-      end
-    end
-  end
-end
-=end
-
 def get_all_flights(airports, visited_airports = [], last_continent = nil, continent_switched = nil)
   airports.each do |airport|
     next if visited_airports.include? airport[:code]
