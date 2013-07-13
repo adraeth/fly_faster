882761590a92115144be3ec0af5bf46f02354c77
Jerzy Mirecki <jerzy.mirecki@gmail.com>
2013-07-13 14:22:11 +0200

    Change visited_airports to use String instead of Hash [4.36]

--- fly.rb
+++ fly.rb
@@ -1,15 +1,15 @@
-def get_all_flights(airports, visited_airports = {}, last_continent = nil, continent_switched = nil)
+def get_all_flights(airports, visited_airports = '', last_continent = nil, continent_switched = nil)
   airports.each do |code, continent|
     next if continent_switched && continent != last_continent
-    next if visited_airports.has_key? code
-    if visited_airports.length == 3
-       puts "#{visited_airports.values.join(' - ')} - #{code}"
+    next if visited_airports.include? code
+    if visited_airports.length == 18
+       puts "#{visited_airports}#{code}"
     else
       switch_continent = continent_switched || last_continent && last_continent != continent
-      get_all_flights(airports, visited_airports.merge({code => code}), continent, switch_continent)
+      get_all_flights(airports, "#{visited_airports}#{code} - ", continent, switch_continent)
     end
   end
-  puts visited_airports.values.join(' - ') unless visited_airports.length == 1
+  puts visited_airports.chomp(' - ') unless visited_airports.length == 6
 end
 
 # RUN
